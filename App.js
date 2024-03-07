60906690417f909123a44305f574998e726977e3
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-03-07 21:34:39 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
+import { useFonts } from 'expo-font';
+import { Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { Inter_700Bold } from '@expo-google-fonts/inter';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   'RobotoBlack' : require("./assets/fonts/Roboto-Black.ttf") 
+  // })
+  let [fontsLoaded, fontError] = useFonts({
+    Inter_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{
+        ...styles.container,
+       
+      }}
+    >
+      <Text style={[styles.blue, styles.bigFont, styles.textFont]}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  blue: {
+    color: "blue",
+  },
+  bigFont: {
+    // fontFamily : 'RobotoBlack',
+    fontSize: 50,
+  },
+  textFont : {
+    fontFamily : 'Inter_700Bold'
+  }
 });
